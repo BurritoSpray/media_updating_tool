e5af1525f9a22fa8776c906e51f0d2e4223e2828
William Goyette <willpower1@live.ca>
2021-10-24 21:23:34 -0400

    Changement pour mettre la classe generique

--- src/net/ddns/goy/tmdb/data/MediaData.java
+++ src/net/ddns/goy/tmdb/data/MediaData.java
@@ -2,7 +2,7 @@ package net.ddns.goy.tmdb.data;
 
 import net.ddns.goy.tmdb.data.DataType;
 
-public class MediaData {
+public class MediaData<T extends MediaData<T>> {
     DataType _mediaType;
 
     public DataType getMediaType(){return _mediaType;}
