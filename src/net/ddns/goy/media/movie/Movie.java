90e8e4ce592863f30583773d7e98a6634c6b43c3
William <willpower1@live.ca>
2021-12-13 18:10:47 -0500

    Now extends Media class

---
+++ src/net/ddns/goy/media/movie/Movie.java
@@ -0,0 +1,30 @@
+package net.ddns.goy.media.movie;
+
+import net.ddns.goy.media.IBaseMedia;
+import net.ddns.goy.media.Media;
+
+public class Movie extends Media {
+    public Movie(String path) {
+        super(path);
+    }
+
+    @Override
+    public void setPath(String path) {
+
+    }
+
+    @Override
+    public void moveTo(String path) {
+
+    }
+
+    @Override
+    public void copyTo(String path) {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
